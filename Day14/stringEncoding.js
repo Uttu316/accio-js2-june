d0f0effb3b6ea08f41407692c2430a35c8e77699
Utkarsh Gupta <Utkarsh.Gupta@setindia.com>
2024-06-27 23:12:01 +0530

    Day 14

---
+++ Day14/stringEncoding.js
@@ -0,0 +1,41 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+function count(str, ans = "") {
+  if (str === "") {
+    // console.log(ans)
+    return 1;
+  }
+
+  const first = parseInt(str[0]);
+
+  if (first === 0) {
+    return 0;
+  }
+
+  const firstChar = String.fromCharCode(first + 96);
+
+  let rest = str.slice(1);
+
+  // console.log(ans)
+  let c = 0;
+  c += count(rest, ans + firstChar);
+
+  const digits = parseInt(str[0] + str[1]);
+  if (digits >= 10 && digits <= 26) {
+    const digitChar = String.fromCharCode(digits + 96);
+
+    const rest = str.slice(2);
+    c += count(rest, ans + digitChar);
+  }
+
+  return c;
+}
+rl.question("", (str) => {
+  console.log(count(str));
+  rl.close();
+});
